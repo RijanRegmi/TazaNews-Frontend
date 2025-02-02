5ba07da0004416a16cef424e2719dab68a6e1079
rijan <rijanregmi111@gmail.com>
2025-02-02 08:57:28 +0545

    Delete TazaNews Folder

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Card from './Card.jsx'
+import Header from './Header.jsx';
+import Food from './Food.jsx';
+import Footer from './Footer.jsx';
+
+function App() {
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Footer/>
+      <Card />
+    </>
+  );
+}
+
+export default App
