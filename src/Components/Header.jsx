1d3159a98909e59ab4a486f774aefaafa39d5292
rijan <rijanregmi111@gmail.com>
2025-02-03 08:17:17 +0545

    Create Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,31 @@
+import profilepic from './../assets/TazaNews.png';
+import './../Style/Header.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import script from './../../script.js';
+
+function Header() {
+    return (
+        <>
+            <sectio className = "header">
+                    <a href="#">
+                    <img src={profilepic} alt=""  className="logo"/>
+                    </a>
+                <div>
+                    <nav>
+                        <ul className="navbar" id="navbar">
+                            <li><a href="#" className="active">Home</a></li>
+                            <li><a href="#News" >News</a></li>
+                            <li><a href="#" >Category</a></li>
+                            <li><a href="#About" >About</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="mobile">
+                    <i class="bi bi-x-lg"></i>
+                </div>
+            </sectio>
+        </>
+    );
+}
+
+export default Header;
